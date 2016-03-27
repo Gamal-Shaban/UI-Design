b7d62fb6195c169a57be813769904294255502f3
Gamal Shaban <gamalshaban@gamals-macbook-pro.local>
2016-03-27 15:36:01 +0200

    Initial Commit

---
+++ Ui Design New/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ui Design New
+//
+//  Created by Gamal Shaban on 3/27/16.
+//  Copyright © 2016 Gamal Shaban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
